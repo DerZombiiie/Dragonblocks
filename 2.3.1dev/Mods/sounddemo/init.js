862c69be863d3452a83d3db56559765993a35868
EliasFleckenstein03 <54945686+EliasFleckenstein03@users.noreply.github.com>
2019-09-06 15:39:39 +0200

    Update init.js

--- 2.3.1dev/Mods/sounddemo/init.js
+++ 2.3.1dev/Mods/sounddemo/init.js
@@ -1,1 +1,21 @@
-
+var mySound = new sound("Mods/sounddemo/sounds/FUCK.mp3");
+var fuck={
+name:"fuck",
+mobStability:true,
+buildStability:true,
+texture:"red",
+groups:["farm"],
+growtime:0,
+growto:"fucksound();",  
+up:0,
+brakedown:0,
+hardness:10,
+inv:{
+	name:"Play Fuck Sound",
+	mod:"sounddemo",
+	},
+}
+function fucksound(){
+mySound.play();
+return air;
+}
